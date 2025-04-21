7f54aff58a11ad70146bd5d0e9140990fd025f43
F. Ricardo <112594292+FelipeRicardo5@users.noreply.github.com>
2025-04-21 17:54:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
-
+import cors from 'cors'
 import app from './app.js';
 import Loaders from "./src/loaders/startDb.js";
 
 app.use(express.json());
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Servidor em funcionamento!');
