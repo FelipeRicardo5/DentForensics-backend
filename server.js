d7f4d1b22cb2d61b506405e94a940e1afa280689
F. Ricardo <112594292+FelipeRicardo5@users.noreply.github.com>
2025-04-21 18:48:58 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ import app from './app.js';
 import Loaders from "./src/loaders/startDb.js";
 
 app.use(express.json());
-app.use(cors())
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
     res.send('Servidor em funcionamento!');
